88e61b9c3e39f9dadb24c20a3f11a4e18d937981
LeonovDmitriy174 <154868677+LeonovDmitriy174@users.noreply.github.com>
2024-06-18 16:22:09 +0500

    Update services.py

--- mailing/services.py
+++ mailing/services.py
@@ -5,62 +5,15 @@ import pytz
 from config import settings
 from mailing.models import MailingSettings, MailingStatus, LOGS_STATUS_CHOICES, MailingMessage
 from django.core.cache import cache
-
 from client.models import Client
 from config.settings import CACHE_ENABLED
-
-
-def send_mailing():
-    zone = pytz.timezone(settings.TIME_ZONE)
-    current_datetime = datetime.now(zone)
-
-    mailings = MailingSettings.objects.filter(first_datetime__lte=current_datetime).filter(
-        setting_status__in=['Create'])
-
-    for mailing in mailings:
-        title = mailing.message.title
-        content = mailing.message.content
-        mailing.setting_status = 'Started'
-        mailing.save()
-        try:
-            server_response = send_mail(
-                subject=title, message=content, from_email=settings.EMAIL_HOST_USER,
-                recipient_list=[client.email for client in mailing.clients.all()], fail_silently=False)
-            if server_response == 1:
-                server_response = 'Письмо успешно отправлено'
-                MailingStatus.objects.create(status=LOGS_STATUS_CHOICES[0][1], mailing_response=server_response,
-                                             mailing=mailing)
-
-            mailing.setting_status = 'Create'
-
-            if mailing.sending == 'daily' and server_response == 1:
-                current_datetime = MailingSettings.objects.get(sending='daily')
-                mailing.first_datetime = current_datetime.first_datetime + timedelta(days=1)
-                mailing.setting_status = 'Create'
-
-            elif mailing.sending == 'weekly' and server_response == 1:
-                current_datetime = MailingSettings.objects.get(sending='weekly')
-                mailing.first_datetime = current_datetime.first_datetime + timedelta(days=7)
-                mailing.setting_status = 'Create'
-
-            elif mailing.sending == 'monthly' and server_response == 1:
-                current_datetime = MailingSettings.objects.get(sending='monthly')
-                mailing.first_datetime = current_datetime.first_datetime + timedelta(days=30)
-                mailing.setting_status = 'Create'
-
-            mailing.save()
-
-        except smtplib.SMTPException as error:
-            MailingStatus.objects.create(status=LOGS_STATUS_CHOICES[1][1], mailing_response=error,
-                                         mailing=mailing)
-            mailing.setting_status = 'Create'
-
+from dateutil.relativedelta import relativedelta
 
 def get_mailings_from_cache():
-    """Получает список клиентов из кэша, если он пустой получает из базы данных"""
+    """Получает список рассылок из кэша, если он пустой получает из базы данных"""
     if not CACHE_ENABLED:
         return MailingSettings.objects.all()
-    key = 'client_list'
+    key = 'mailing_list'
     mailings = cache.get(key)
     if mailings is not None:
         return mailings
@@ -70,13 +23,69 @@ def get_mailings_from_cache():
 
 
 def get_messages_from_cache():
-    """Получает список клиентов из кэша, если он пустой получает из базы данных"""
+    """Получает список сообщений из кэша, если он пустой получает из базы данных"""
     if not CACHE_ENABLED:
         return MailingMessage.objects.all()
-    key = 'client_list'
+    key = 'message_list'
     messages = cache.get(key)
     if messages is not None:
         return messages
     messages = MailingMessage.objects.all()
     cache.set(key, messages)
     return messages
+
+
+def send_mailing():
+    """Функция отправки письма"""
+    # Определяем текущее время
+    zone = pytz.timezone(settings.TIME_ZONE)
+    current_time = datetime.now(zone)
+
+    # Собираем рассылки, которые необходимо отправить
+    mailing_settings = MailingSettings.objects.filter(first_datetime__lte=current_time).filter(
+        setting_status__in=['Create', 'Started'])
+    for mailing in mailing_settings:
+        # Устанавливаем время следующей рассылки, если оно еще не определено
+        if mailing.first_datetime is None:
+            mailing.first_datetime = current_time
+        # Подготавливаем данные для отправки письма
+        title = mailing.message.title
+        content = mailing.message.content
+        mailing.setting_status = 'Started'
+        mailing.save()
+        try:
+            # Проверяем, не истекло ли время рассылки
+            if mailing.end_time < mailing.first_datetime:
+                mailing.first_datetime = current_time
+                mailing.settings_status = 'Done'
+                mailing.save()
+                continue
+            # Проверяем, требуется ли отправить рассылку
+            if mailing.first_datetime <= current_time:
+                # Отправляем письмо
+                server_response = send_mail(
+                    subject=title,
+                    message=content,
+                    from_email=settings.EMAIL_HOST_USER,
+                    recipient_list=[client.email for client in mailing.clients.all()],
+                    fail_silently=False,
+                )
+                if server_response == 1:
+                    server_response = 'Сообщение отправлено'
+                # Создаем модель статуса рассылки
+                MailingStatus.objects.create(status='success', mailing_response=server_response, mailing=mailing)
+
+                # Определяем время следующей рассылки
+                if mailing.sending == 'Daily':
+                    mailing.first_datetime = current_time + timedelta(days=1)
+
+                elif mailing.sending == 'Weekly':
+                    mailing.first_datetime = current_time + timedelta(days=7)
+
+                elif mailing.sending == 'Monthly':
+                    mailing.first_datetime = current_time + relativedelta(months=1)
+
+            mailing.save()
+
+        except smtplib.SMTPException as error:
+            MailingStatus.objects.create(status='fail', mailing_response=error, mailing=mailing)
