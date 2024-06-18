00778689f928d6d2b2e0183c4f4bc027a2cbe7ba
LeonovDmitriy174 <154868677+LeonovDmitriy174@users.noreply.github.com>
2024-06-18 22:54:02 +0500

    Update runapscheduler.py

--- mailing/management/commands/runapscheduler.py
+++ mailing/management/commands/runapscheduler.py
@@ -43,7 +43,7 @@ class Command(BaseCommand):
 
         scheduler.add_job(
             send_mailing,
-            trigger=CronTrigger(second="*/10"),  # Every 10 seconds
+            trigger=CronTrigger(minute="*/1"),  # Every 1 minute
             id="send_mailing",  # The `id` assigned to each job MUST be unique
             max_instances=1,
             replace_existing=True,
